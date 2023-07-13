b8fb7c5ebbbdb928ee60885d74f8bd3b0192fe36
Folorunso Olorunfemi Emmanuel <femite58@gmail.com>
2023-07-13 02:18:48 +0100

    done

--- js/header.js
+++ js/header.js
@@ -5,7 +5,8 @@ document.write(`<!DOCTYPE html>
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>David Shittu</title>
+        <title>${title}</title>
+        <meta name="description" content="${desc}" />
         <link rel="stylesheet" href="${path}style.css" />
         <link rel="icon" href="${path}assets/images/favicon.png"/>
     </head>
