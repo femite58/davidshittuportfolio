19f7b62b848ba21dee0e302062ac3b7350afcc15
Folorunso Olorunfemi Emmanuel <femite58@gmail.com>
2023-08-13 20:44:34 +0100

    done

--- js/worksFooter.js
+++ js/worksFooter.js
@@ -3,12 +3,30 @@ document.write(`
 <img src="../assets/images/giphy.gif" alt="The End" />
 </a>
 <div id="pageCtrl">
-<a href="${prev}" ${prev.match(/https/) ? 'target="_blank"' : ''} class="eachCtrl ${prev ? '' : 'hidden'}">
-    <span class="svgIcon-smallArrowLeft"></span>
-    <span>Previous</span>
+<a href="${prev?.url}" ${
+    prev?.url?.match(/https/) ? 'target="_blank"' : ''
+} class="eachCtrl ${prev?.url ? '' : 'hidden'}">
+    <div class="imgCont">
+        <img src="${prev?.img}"/>
+    </div>
+    <div class="txtPart" style="background-color: ${prev?.bg};">
+        <div class="action">
+            <span class="svgIcon-smallArrowLeft"></span>
+            <span>Previous</span>
+        </div>
+        <div class="pageTitle">${prev?.txt}</div>
+    </div>
 </a>
-<a href="${next}" class="eachCtrl ${next ? '' : 'hidden'}">
-    <span>Next</span>
-    <span class="svgIcon-smallArrowRight"></span>
+<a href="${next?.url}" class="eachCtrl ${next?.url ? '' : 'hidden'}">
+    <div class="imgCont">
+        <img src="${next?.img}"/>
+    </div>
+    <div class="txtPart" style="background-color: ${next?.bg};">
+        <div class="action">
+            <span>Next</span>
+            <span class="svgIcon-smallArrowRight"></span>
+        </div>
+        <div class="pageTitle">${next?.txt}</div>
+    </div>
 </a>
 </div>`);
